65dce17203a5d1f56c93d10fbe31e6af4617e53a
gerardo cruz <kenetch@hotmail.com>
2019-12-16 12:54:33 -0600

    Initial Commit

---
+++ Fintonic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fintonic
+//
+//  Created by Juan Gerardo Cruz on 12/16/19.
+//  Copyright © 2019 Juan Gerardo Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
